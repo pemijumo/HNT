3b47006f5e8966167f32aebbd4d5bf4cbdc9b227
pemijumo <pemijumo@gmail.com>
2017-10-12 10:19:02 -0500

    111

--- js/actions.js
+++ js/actions.js
@@ -18,17 +18,13 @@ var fn = {
     },
 
     getUtilidad: function (){
-        alert ("Hola pemijumo");
-        //var onSuccess = function (position) {
-        //    alert('Latitude: ' + position.coords.latitude + '\n' +
-        //        'Longitude: ' + position.coords.longitude + '\n' +
-        //        'Altitude: ' + position.coords.altitude + '\n' +
-        //        'Accuracy: ' + position.coords.accuracy + '\n' +
-        //        'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
-        //        'Heading: ' + position.coords.heading + '\n' +
-        //        'Speed: ' + position.coords.speed + '\n' +
-        //        'Timestamp: ' + position.timestamp + '\n');
-        //};
+        navigator.geolocation.getCurrentPosition(
+            function (pocicion) {
+                var LatitudCarga = pocicion.coords.longitude;
+                var LongitudCarga = pocicion.coords.latitude;
+
+                alert(LatitudCarga);
+            });
 
     },
 
